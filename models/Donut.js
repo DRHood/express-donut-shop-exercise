b940455369d2c43c71727d031c71ea27a102cc0c
DRHood <darryl.r.hood@gmail.com>
2020-02-18 09:55:09 -0500

    created donut schema

--- models/Donut.js
+++ models/Donut.js
@@ -1,7 +1,19 @@
-// requirements: require db/connection as 'mongoose'
+// import { Schema } from "mongoose";
 
+// requirements: require db/connection as 'mongoose'
+const mongoose = require('db/connection');
+const Schema = mongoose.Schema;
 
 // create your donut schema:
-
+const Donut = new Schema({
+// Schema fields here
+// name, description, img, price, qty
+name: String,
+description: String,
+img: String,
+price: Number,
+qty: Number
+});
 
 // export the donut model with module.exports
+module.exports = mongoose.model('Donut', Donut);
